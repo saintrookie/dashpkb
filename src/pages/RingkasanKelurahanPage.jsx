ef8153d711e07a8571903363d095a23f33f17d23
agi noer <itpd-metrocom.agi@btn.co.id>
2026-08-28 11:30:22 +0700

    update fixing

--- src/pages/RingkasanKelurahanPage.jsx
+++ src/pages/RingkasanKelurahanPage.jsx
@@ -76,6 +76,7 @@ export default function RingkasanKelurahanPage() {
               title="RANKING KELURAHAN BERDASARKAN COLLECTION RATE"
               rows={rankedRows}
               showMoreLink={false}
+              showRevenueBreakdown={isAdmin}
             />
           )}
         </div>
